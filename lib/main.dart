eae6257a4ac172c286a017aab0e62420ab449dc7
sahil <aorking.shp007@gmail.com>
2022-08-27 14:39:24 +0530

    launch an icon

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Database',
       debugShowCheckedModeBanner: false,
       home: HiveDatabaseHomeScreen(),
     );
